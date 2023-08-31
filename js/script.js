9e9ecf16a3ad0161200898399db5886a99781d4c
ahkamulbd <ahkamulbd@gmail.com>
2023-08-31 15:09:09 +0600

    Scope and var, let & const addressed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log('Addressing JS Scope');
+//console.log('Addressing JS Scope');
+
+/* ==== Lexical Scope ====== */
+
+function parentFunction() {
+    let a = 10;
+    function childFunction() {
+        let b = 20;
+        let sum = a + b;
+        //console.log(`Sum = ${sum}`);
+    }
+    childFunction();
+}
+parentFunction();
+
+/* ==== var, let & const ====== */
+
+if (true) {
+    var varVariable = 'This is var';
+}
+console.log(varVariable);
+
+if (true) {
+    let letVariable = 'This is let';
+}
+console.log(letVariable);
+
+// N.B. let is block scope, but var is functional scope
